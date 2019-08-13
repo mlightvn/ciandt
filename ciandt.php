fd14ea30b4ac6f7c4a07aa43424dd7c8b7d3cec6
Nguyen Ngoc Nam <ngocnammoney@gmail.com>
2019-08-13 14:33:18 +0900

    shorten result

--- ciandt.php
+++ ciandt.php
@@ -4,15 +4,30 @@ test();
 
 function test()
 {
+	// testcase 1
 	$totalMemory=10; // MB
 	$foregroundApps = [[1,2], [2,5], [3,8], ];
 	$backgroundApps = [[1,4], ];
 
 	$resultArr = ciandt($totalMemory, $foregroundApps, $backgroundApps);
 
+	echo "TESTCASE 1\n";
 	foreach ($resultArr as $key => $valueArr) {
 		echo "[" . $valueArr[0] . "," . $valueArr[1] . "]\n";
 	}
+
+	// testcase 2
+	$totalMemory=20; // MB
+	$foregroundApps = [[1,10], [2,15], [3,17], ];
+	$backgroundApps = [[1,3], [2, 5], ];
+
+	$resultArr = ciandt($totalMemory, $foregroundApps, $backgroundApps);
+
+	echo "\nTESTCASE 2\n";
+	foreach ($resultArr as $key => $valueArr) {
+		echo "[" . $valueArr[0] . "," . $valueArr[1] . "," . $valueArr[2] . "]\n";
+	}
+
 }
 
 function ciandt($totalMemory=0, $foregroundApps = [], $backgroundApps = [])
@@ -25,13 +40,24 @@ function ciandt($totalMemory=0, $foregroundApps = [], $backgroundApps = [])
 			if(count($foregroundApp) == 2){
 				if(count($foregroundApp) == 2){
 					$maxMemory = $foregroundApp[1] + $backgroundApp[1];
-					if($maxMemory < $totalMemory){
-						$resultArr[] = [$foregroundApp[0], $backgroundApp[0]];
+					if($maxMemory <= $totalMemory){
+						if(isset($resultArr[$foregroundApp[0]])){
+							if($resultArr[$foregroundApp[0]][2] < $maxMemory){
+								$resultArr[$foregroundApp[0]] = [$foregroundApp[0], $backgroundApp[0], $maxMemory];
+							}
+						}else{
+							$resultArr[$foregroundApp[0]] = [$foregroundApp[0], $backgroundApp[0], $maxMemory];
+						}
 					}
 				}
 			}
 		}
 	}
+
+	// $tempArr = $resultArr;
+	// foreach ($resultArr as $key => $record) {
+	// 	$tempArr[$resultArr[$key][0]] = $resultArr
+	// }
 
 	return $resultArr;
 }
